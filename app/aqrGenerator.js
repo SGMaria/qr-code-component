838ca93655d026cce9b450fcc8ef9532e7d5f64b
Maria Luque <33879614+SGMaria@users.noreply.github.com>
2023-09-06 18:50:31 -0300

    Add files via upload

--- app/aqrGenerator.js
+++ app/aqrGenerator.js
@@ -1,30 +1,59 @@
-/*var qrcode = new QRCode("test", {
-    text: "http://jindo.dev.naver.com/collie",
-    width: 128,
-    height: 128,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
+  /* Creates the QR code. Has default values*/
+  var qr = new QRCode(document.getElementById("qrcode"), {
+    colorDark : "#FFFFFF",
+    colorLight : "#00B9E8",
     correctLevel : QRCode.CorrectLevel.H
 });
 
-
-var qrcode = new QRCode("qrcode");
-
-function makeCode () {    
-  var elText = document.getElementById("qrcode");
-  
-  if (!elText.value) {
-    alert("Input a text");
-    elText.focus();
-    return;
+  /* Gets the colors from the inputs*/
+  function getColors(){
+    let color1 = document.getElementById("qrColor1").value;
+    let color2 = document.getElementById("qrColor2").value;
+    qr._htOption.colorDark = color1;
+    qr._htOption.colorLight = color2;
   }
+
+  function getLink(){
+    let link = document.getElementById("qrlink").value;
+    getColors();
+    generateQR(link);
+  }
+
+  /* Generates the QR code */
+  function generateQR(link){
+    let defaultLink = "https://google.com";
+    if (!link){
+        link = defaultLink;
+        document.getElementById("qrlink").value = defaultLink;
+    } 
+    qr.makeCode(link)
+  }
+
+  /* Gets the src of the qr img and starts the download of said img */
+  function getQRImg(){
+    let src = document.getElementById("qrcode").children[1].getAttribute("src");
+    downloadImage(src);
+  }
+
+  /* To download the QR Image */
+  async function downloadImage(imageSrc, nameOfDownload = 'my-qrcode.png') {
+    
+    const response = await fetch(imageSrc);
+    const blobImage = await response.blob();
+    const href = URL.createObjectURL(blobImage);
   
-  qrcode.makeCode(elText.value);
-}
+    const anchorElement = document.createElement('a');
+    anchorElement.href = href;
+    anchorElement.download = nameOfDownload;
+  
+    document.body.appendChild(anchorElement);
+    anchorElement.click();
+  
+    document.body.removeChild(anchorElement);
+    window.URL.revokeObjectURL(href);
+  }
 
-makeCode();
-*/
-
+  /* Starts the funtions by pressing the key Enter*/
   document.getElementById("qrlink").addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
       event.preventDefault();
@@ -32,26 +61,9 @@ makeCode();
     }
   });
 
-  const qr = new QRCode(document.getElementById("qrcode"), {
-    colorDark : "#FFFFFF",
-    colorLight : "#00B9E8",
-    correctLevel : QRCode.CorrectLevel.H
-});
-  function getLink(){
-    let link = document.getElementById("qrlink").value;
-    generateQR(link);
-  }
-
-  function generateQR(link){
-    let defaultLink = "https://google.com"
-    if (!link){
-        link = defaultLink;
-        document.getElementById("qrlink").value = defaultLink;
-    } 
-    console.log(link);
-    qr.makeCode(link);
-  }
-
+  /* Calls */
   const qrlinkbutton = document.getElementById("qrlinkbutton");
-  qrlinkbutton.addEventListener("click", getLink)
-  document.addEventListener("load", generateQR(""))
+  qrlinkbutton.addEventListener("click", getLink);
+  document.addEventListener("load", generateQR(""));
+  const qrdownloadbutton = document.getElementById("qrdownload");
+  qrdownloadbutton.addEventListener("click", getQRImg);
